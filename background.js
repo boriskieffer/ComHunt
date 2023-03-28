2bf0dc31bd1af67ec507a087149989b02fcb3db3
Boris KIEFFER <boris.kieffer@mail.fr>
2023-03-28 21:05:10 +0200

    Fixed background.js not injecting on refresh

--- background.js
+++ background.js
@@ -1,27 +1,31 @@
-console.log('bg script');
-let tabInstances = [];
-chrome.tabs.onUpdated.addListener( async function (tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete') {
-        if (tabInstances.indexOf(tabId) == -1) {
-            await browser.tabs.executeScript(tab.id, {file: 'comhunt.js'}).then(async response => {
-                console.log('Injected comhunt.js');
-                await browser.tabs.sendMessage(tab.id, { comhunt_command: 'setTabId', comhunt_data: {new_tabId: tabId} });
-            });
-            
-            tabInstances.push(tabId);
-        } else {
-            if (changeInfo.url){
-                await browser.tabs.sendMessage(tab.id, {
-                    comhunt_command: 'REFRESH_INSTANCE',
-                    comhunt_data: {
-                        target_tabId: tab.id,
-                        newUrl: tab.url
-                    }
-                });
-            } else {
-                tabInstances.pop(tabId);
+let tabInstances = {};
+chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
+    if (new URL(tab.url).hostname == 'www.youtube.com') {
+        if (changeInfo.status !== undefined) {
+            if (changeInfo.status == 'complete') {
+                // if the tab instance doens't exist or updated tab instance url equals the stored tab instance url,
+                // then it's probably an initial load or a refresh
+                if (!tabInstances[tabId] || tabInstances[tabId] == tab.url) {
+                    await browser.tabs.executeScript(tab.id, {file: 'comhunt.js'}).then(async response => {
+                        await browser.tabs.sendMessage(tab.id, { comhunt_command: 'setTabId', comhunt_data: {new_tabId: tabId} });
+                    });
+                } else {
+                    // else, send notification to the content scripts
+                    await browser.tabs.sendMessage(tab.id, {
+                        comhunt_command: 'REFRESH_INSTANCE',
+                        comhunt_data: {
+                            target_tabId: tab.id,
+                            newUrl: tab.url
+                        }
+                    });
+                }
+    
+                tabInstances[tabId] = tab.url;
             }
         }
     }
 });
 
+chrome.tabs.onRemoved.addListener(function(tabId) {
+    tabInstances.pop(tabId);
+})
